8469a80d129e8627245868af3e825e6b5e3ecc64
Marceli Janowski <marceli@mjdesign.pl>
2019-07-04 18:12:29 +0200

    Move read steam back to the controller

--- src/controller/anagram.ts
+++ src/controller/anagram.ts
@@ -1,31 +1,46 @@
 import { Response, Request, NextFunction } from "express-serve-static-core";
-import * as es from 'event-stream';
-import * as fs from 'fs';
-import * as path from 'path';
-import { isAnagram } from '../lib/utils';
+import * as es from "event-stream";
+import * as fs from "fs";
+import * as path from "path";
+import { isAnagram } from "../lib/utils";
 
-const readStream: any = fs.createReadStream(path.join(`${__dirname}../../data/wordlist.txt`), {
-  encoding: 'utf8', 
-  highWaterMark: 1024
-});
-
-export const getAnagram = (request: Request, response: Response,  next: NextFunction): void => {
+export const getAnagram = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void => {
   try {
     const { params } = request;
     const { q } = params;
-    const anagramsArray = q.split(',');
-    let anagrams: { string: [] }[] = anagramsArray.reduce((acc: [], item: string) => { acc[`${item}`] = []; return acc;} , {});
-    readStream
-      .pipe(es.split())
-      .pipe(es.mapSync((line: string) => anagramsArray.map((item: string) => {
-        anagrams[item] =  line.toLowerCase() !== item.toLowerCase() && isAnagram(line, item) ? [...anagrams[item],...[line]] : anagrams[item];
-    })))
-    readStream.on('end', () => {
-      response
-        .status(200)
-        .json(anagrams);
+    const anagramsArray = q.split(",");
+    let anagrams: { string: [] }[] = anagramsArray.reduce(
+      (acc: [], item: string) => {
+        acc[`${item}`] = [];
+        return acc;
+      },
+      {}
+    );
+    const readStream: any = fs.createReadStream(
+      path.join(`${__dirname}../../data/wordlist.txt`),
+      {
+        encoding: "utf8",
+        highWaterMark: 1024
+      }
+    );
+    readStream.pipe(es.split()).pipe(
+      es.mapSync((line: string) =>
+        anagramsArray.map((item: string) => {
+          anagrams[item] =
+            line.toLowerCase() !== item.toLowerCase() && isAnagram(line, item)
+              ? [...anagrams[item], ...[line]]
+              : anagrams[item];
+        })
+      )
+    );
+    readStream.on("end", () => {
+      response.status(200).json(anagrams);
     });
-    readStream.on('error', (error: Error) => {
+    readStream.on("error", (error: Error) => {
       throw new Error(error.message);
     });
   } catch (error) {
